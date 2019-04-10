0db1bc3b4a2846c58effa1194bf0d243d4215d79
sbboard <colin.buffum@gmail.com>
2019-04-10 16:01:54 -0600

    finished controllers (for now!!)

--- backend_nodejs/controllers/series.controller.js
+++ backend_nodejs/controllers/series.controller.js
@@ -1,6 +1,5 @@
 const Series = require('../models/series.model')
 
-//DONE
 exports.series_list = (req, res, next) => {
     Series.find({}).sort('debutYear').select('name -_id').exec((err,personList) => {
         if (err) return next(err)
@@ -8,7 +7,6 @@ exports.series_list = (req, res, next) => {
     })
 }
 
-//DONE
 exports.member_list = (req, res, next) => {
     Series.find({name:req.params.series}).select('characters').exec((err,personList) => {
         if (err) return next(err)
@@ -16,15 +14,18 @@ exports.member_list = (req, res, next) => {
     })
 }
 
-//list all characters that have been assigned to a series
 exports.all_chars = (req, res, next) => {
-    Product.find({category:req.params.cat}).sort('-date').exec((err,personList) => {
+    Series.find({}).select('characters').exec((err,personList) => {
         if (err) return next(err)
-        res.send(personList)
+        let bigArray = personList
+        let newArray = []
+        for(let i=0;i<bigArray.length;i++){
+            newArray = newArray.concat(bigArray[i].characters)
+        }
+        res.send(newArray)
     })
 }
 
-//DONE
 exports.series_create = (req, res, next) => {
     let product = new Series({
         name: req.body.name,
@@ -41,7 +42,6 @@ exports.series_create = (req, res, next) => {
     })
 }
 
-//DONE
 exports.person_assign = (req, res, next) => {
     Series.updateOne({name:req.body.name}, { $push: { characters: req.body.character}},
         (err) => {
@@ -49,16 +49,13 @@ exports.person_assign = (req, res, next) => {
         res.send("Character Assigned")
     })
 }
-
-//remove person from series
 exports.person_remove = (req, res, next) => {
-    Product.findByIdAndRemove(req.params.id, (err) => {
+    Series.findByIdAndRemove(req.params.id, (err) => {
         if (err) return next(err)
         res.send('Deleted successfully')
     })
 }
 
-//DONE
 exports.everything = (req, res, next) => {
     Series.find({}).sort('debutYear').exec((err,personList) => {
         if (err) return next(err)
--- backend_nodejs/routes/product.route.js
+++ backend_nodejs/routes/product.route.js
@@ -6,11 +6,11 @@ const series_controller = require('../controllers/series.controller')
 //list of series
 router.get('/list/series', series_controller.series_list)
 
-//members of specific series
-router.get('/:series/characters', series_controller.member_list)
-
 //list all characters that have been assigned to a series
 router.get('/list/characters', series_controller.all_chars)
+
+//members of specific series
+router.get('/:series/characters', series_controller.member_list)
 
 //create
 router.post('/create', series_controller.series_create)
@@ -18,7 +18,7 @@ router.post('/create', series_controller.series_create)
 //assign person to series
 router.put('/assign', series_controller.person_assign)
 
-//remove person from series
+//delete series
 router.delete('/:id/delete', series_controller.person_remove)
 
 //everything
