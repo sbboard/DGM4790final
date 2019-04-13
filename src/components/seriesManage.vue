0c4a62a464c7ce5a6092b15e6e3c0336ee1dc4a0
sbboard <colin.buffum@gmail.com>
2019-04-12 21:45:54 -0600

    list of series entry not created via API

--- src/components/seriesManage.vue
+++ src/components/seriesManage.vue
@@ -1,17 +1,28 @@
 <template>
     <div>Series Manager
-
-    <h2>Create Series</h2>
-    name = input
-    debut year = input
-    first smash (dropdown all smash titles name=smash 64 value=1)
-    nintendo property? radio button
+        <h2>Create Series</h2>
+        name = input
+        debut year = input
+        first smash (dropdown all smash titles name=smash 64 value=1)
+        nintendo property? radio button
+        <select>
+            <option :value="item.name" v-for="(item, id) in listOfSeries" :key="`${id}`">{{item.name}}</option>
+        </select>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    
+    data(){
+        return{
+            listOfSeries: []
+        }
+    },
+    mounted () {
+        axios.get(`http://192.168.56.1:8666/api/list/series`).then(response => (this.listOfSeries = response.data))
+    },
 }
 </script>
 
