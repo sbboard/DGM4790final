75d8f53f008fdd3b01107aef672bd9df7457ef48
sbboard <colin.buffum@gmail.com>
2019-04-17 15:29:11 -0600

    viewer prints info from graphql

--- src/components/viewer.vue
+++ src/components/viewer.vue
@@ -1,9 +1,36 @@
 <template>
-    <div>Viewer</div>
+    <div>
+        <div v-for="item in characters" :key="item.id" :id="item.id">
+            <template v-if="item.thumbnailImg != null">
+                <img :src="item.thumbnailImg" :alt="item.name"/>
+            </template>
+            <template v-else>
+                <img src="https://www.my-vb.com/img/assets/missing_image.jpg" :alt="item.name"/>
+            </template>
+            <div class="crossBar">Name: {{item.name}}</div> 
+            <div class="crossBar">Series: <span>Undefined</span></div>            
+        </div>
+    </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
 export default {
+    data(){
+        return{
+            characters: []
+        }
+    },
+    apollo: {
+        // Simple query that will update the 'hello' vue property
+        characters: gql`query getCharacterNames{
+            characters{
+                name
+                id
+                thumbnailImg
+            }
+        }`,
+    },
     
 }
 </script>
