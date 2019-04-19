c45d689751cbba9ff65dd511aa6e87fb61d237b1
sbboard <colin.buffum@gmail.com>
2019-04-19 17:15:28 -0600

    series names now applied

--- src/components/viewer.vue
+++ src/components/viewer.vue
@@ -16,21 +16,17 @@
 </template>
 
 <script>
+import axios from 'axios'
 import gql from 'graphql-tag'
 export default {
     data(){
         return{
             characters: [],
-            cardCount: 3
-        }
-    },
-    methods: {
-        addNum(){
-            this.cardCount = this.cardCount + 1
+            cardCount: 3,
+            seriesReturn: [],
         }
     },
     apollo: {
-        // Simple query that will update the 'hello' vue property
         characters: gql`query getCharacterNames{
             characters{
                 name
@@ -39,7 +35,24 @@ export default {
             }
         }`,
     },
-
+    watch:{
+        characters(){
+            axios.get(`http://192.168.56.1:8666/api/`).then(response => (this.seriesReturn = response.data))
+            .then(this.theLoop)
+        }
+    },
+    methods:{
+        theLoop(){
+            for(let i=0;i<this.seriesReturn.length;i++){
+                const seriesName = this.seriesReturn[i].name
+                const characterArray = this.seriesReturn[i].characters
+                for(let i=0;i<characterArray.length;i++){
+                    const characterName = characterArray[i]
+                    document.getElementById(characterName).innerHTML = seriesName
+                }
+            }
+        }
+    }
 }
 </script>
 
